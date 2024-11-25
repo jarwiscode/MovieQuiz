854f00ae8b9792936ae35dca92649be5a24d9753
jarwiscode <dz.ibragimov@yandex.ru>
2024-11-25 11:53:00 +0400

    Обновление файлов

---
+++ MovieQuiz/Helpers/Fonts.swift
@@ -0,0 +1,14 @@
+//
+//  Fonts.swift
+//  MovieQuiz
+//
+//  Created by Dzhamal Ibragimov on 23/11/2024.
+//
+
+import Foundation
+import UIKit
+
+private struct Fonts {
+    let YSDisplayBold: String = "YSDisplay-Bold"
+    let YSDisplayMedium: String = "YSDisplay-Medium"
+}
